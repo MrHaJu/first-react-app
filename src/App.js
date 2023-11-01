56926dd4957fbc6da6d2e06ab3d3c576709d17d5
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-11-01 17:38:34 +0100

    stateful greeting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+// import FunctionalGreeting from './components/FunctionalGreetings'; //
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StatefulGreeting greeting="I'm a stateful class Comonent!"/>
+      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+      
     </div>
   );
 }
