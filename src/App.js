d7f231e87aaeba9c7121bba947b3966d09ce827a
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-11-09 10:34:11 +0100

    call sidebar and Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 // import FunctionalGreeting from './components/FunctionalGreetings'; //
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import Sidebar from "./components/Sidebar";
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className="App">
       <StatefulGreeting greeting="I'm a stateful class Comonent!"/>
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+      <NavBarSimple />
+      <Sidebar />
       
     </div>
   );
