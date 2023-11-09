daea434df65a93609fe14ec5cbb0bcd6a5f47a90
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-11-09 15:33:43 +0100

    import EventsClass, EventsFunctional, EventBinding

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import Sidebar from "./components/Sidebar";
 import NavBarSimple from './components/NavBarSimple';
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
+import EventBinding from "./components/EventBinding";
+
 
 function App() {
   return (
     <div className="App">
+      <header>
       <StatefulGreeting greeting="I'm a stateful class Comonent!"/>
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
-      <NavBarSimple />
-      <Sidebar />
+      </header>
+      <nav>
+        <NavBarSimple />
+      </nav>
+      <article>
+        <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+      </article>
+      <aside>
+        <Sidebar />
+      </aside>
+      <extra>
+        <EventsFunctional />
+      <EventsClass />
+      <EventBinding />
+      </extra>
       
+
     </div>
   );
 }
